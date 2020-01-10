53b2f83dea8a72820da7a346a30d82138aaad987
gogabego <45129184+gogabego@users.noreply.github.com>
2020-01-10 11:31:17 -0600

    Create experimental.py
    This is a place for experimentation with mesh files

---
+++ experimental.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 10:48:33 2020
+worked tutorial from https://pypi.org/project/numpy-stl/
+@author: gpinnell
+"""
+
+import numpy
+import bpy
+import bmesh
+from stl import mesh
+import math
+
+# Using an existing stl file:
+#your_mesh = mesh.Mesh.from_file('some_file.stl')
+
+# Or creating a new mesh (make sure not to overwrite the `mesh` import by
+# naming it `mesh`):
+VERTICE_COUNT = 100
+# Create 3 faces of a cube
+data = numpy.zeros(6, dtype=mesh.Mesh.dtype)
+
+# Top of the cube
+data['vectors'][0] = numpy.array([[0, 1, 1],
+                                  [1, 0, 1],
+                                  [0, 0, 1]])
+data['vectors'][1] = numpy.array([[1, 0, 1],
+                                  [0, 1, 1],
+                                  [1, 1, 1]])
+# Front face
+data['vectors'][2] = numpy.array([[1, 0, 0],
+                                  [1, 0, 1],
+                                  [1, 1, 0]])
+data['vectors'][3] = numpy.array([[1, 1, 1],
+                                  [1, 0, 1],
+                                  [1, 1, 0]])
+# Left face
+data['vectors'][4] = numpy.array([[0, 0, 0],
+                                  [1, 0, 0],
+                                  [1, 0, 1]])
+data['vectors'][5] = numpy.array([[0, 0, 0],
+                                  [0, 0, 1],
+                                  [1, 0, 1]])
+
+# Since the cube faces are from 0 to 1 we can move it to the middle by
+# substracting .5
+data['vectors'] -= .5
+
+# Generate 4 different meshes so we can rotate them later
+meshes = [mesh.Mesh(data.copy()) for _ in range(4)]
+
+# Rotate 90 degrees over the Y axis
+meshes[0].rotate([0.0, 0.5, 0.0], math.radians(90))
+
+# Translate 2 points over the X axis
+meshes[1].x += 2
+
+# Rotate 90 degrees over the X axis
+meshes[2].rotate([0.5, 0.0, 0.0], math.radians(90))
+# Translate 2 points over the X and Y points
+meshes[2].x += 2
+meshes[2].y += 2
+
+# Rotate 90 degrees over the X and Y axis
+meshes[3].rotate([0.5, 0.0, 0.0], math.radians(90))
+meshes[3].rotate([0.0, 0.5, 0.0], math.radians(90))
+# Translate 2 points over the Y axis
+meshes[3].y += 2
+
+#added by gabe - bmesh 
+bm = bmesh.new()
+total_mesh = bpy.data.meshes.new("")
+
+for item in meshes:
+    bmesh.from_mesh(item)
+    
+    
+bm.to_mesh(total_mesh)
+
+total_mesh.save('new_stl_file.stl')
